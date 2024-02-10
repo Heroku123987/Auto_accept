9a969d8ccdb3a34696b4f698b5f844158f603a56
Heroku123987 <159604503+Heroku123987@users.noreply.github.com>
2024-02-10 20:08:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ async def approve(_, m : Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id,img, "**Hello {}!\ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ᴛᴏ ᴊᴏɪɴ {} ɪs ᴀᴘᴘʀᴏᴠᴇᴅ. \n\n__Powerd By : @Ghost_Botz __**".format(m.from_user.mention, m.chat.title))
+        await app.send_video(kk.id,img, "**Hello {}!\n ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ᴛᴏ ᴊᴏɪɴ {} ɪs ᴀᴘᴘʀᴏᴠᴇᴅ. \n\n__Powerd By : @Ghost_Botz __**".format(m.from_user.mention, m.chat.title))
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
